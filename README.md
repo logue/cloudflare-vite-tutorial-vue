14df45a647d266d5cf013f916f707088ca1eab5c
logue <logue@hotmail.co.jp>
2025-03-26 22:17:43 +0900

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cloudflare vite tutorial vue
+
+Cloudflare [Wrangler](https://developers.cloudflare.com/workers/wrangler/) + vue demo program.
+
+```
+pnpm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const name = ref<string>("unknown");
+
+const fetchName = async () => {
+  const res = await fetch("/api/");
+  const data = await res.json();
+  name.value = data.name;
+};
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="card">
+    <button @click="fetchName" aria-label="get name">
+      Name from API is: {{ name }}
+    </button>
+    <p>Edit <code>api/index.ts</code> to change the name</p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
